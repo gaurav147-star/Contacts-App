6acf3d414d2c1b07a7d40d170c68fb4c438749a6
gaurav147-star <gauravgupta9158@gmail.com>
2021-12-29 17:26:53 +0530

    toggle and alert added for light and dark mode

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./App.css"
 import { ToastContainer } from 'react-toastify';
 import Navbar from './components/Navbar';
@@ -10,14 +10,43 @@ import {
 import Home from './components/Home';
 import AddContact from './components/AddContact';
 import EditContact from './components/EditContact';
+import Alert from './components/Alert';
 
 const App = () => {
+
+  const [mode, setmode] = useState('light');
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500)
+  }
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setmode('dark');
+      document.body.style.backgroundColor = 'rgba(0, 0, 0, 0.7)';
+      showAlert("Dark mode has been enabled", "success");
+    }
+    else {
+      setmode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("light mode has been enabled", "success");
+    }
+  }
+
 
   return (
     <Router>
       <div className='App'>
         <ToastContainer />
-        <Navbar />
+        <Navbar toggleMode={toggleMode} />
+        <Alert alert={alert} />
         <Switch>
           <Route exact path="/">
             <Home />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import { Link } from "react-router-dom";
 
-const Navbar = () => {
+const Navbar = (props) => {
     return (
-        <div className="col-md-12 bg-dark py-2">
-            <nav className="navbar bg-dark navbar-dark">
-                <Link to={"/"} className="navbar-brand ml-5">
-                    React Redux Contact Book
-                </Link>
-            </nav>
-        </div>
+        // <div className="col-md-12 bg-dark py-2">
+        <nav className="navbar bg-dark navbar-dark py-3">
+            <Link to={"/"} className="navbar-brand m-auto">
+                React Redux Contacts Book
+            </Link>
+            <div className="form-check form-switch">
+                <input className="form-check-input" onClick={props.toggleMode} type="checkbox" id="flexSwitchCheckDefault" />
+                <label className={`form-check-label text-${props.mode === 'light' ? 'dark' : 'light'}`}>Enabel {props.mode === 'light' ? 'dark' : 'light'} Mode</label>
+            </div>
+        </nav>
+        // </div>
     )
 }
 
